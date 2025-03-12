9ad42f70a6e3f5c516cc3ed69930d1475d5bd726
MariiaRoglev <Mariarogaleva06@gmail.com>
2025-03-12 22:15:01 +0200

    create project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Body, Controller, Delete, Get, Param, Patch, Post, Put} from '@nestjs/common';
+import {AppService, Todo} from './app.service';
+import {CreateTodoDto} from './create-todo.dto';
+import {UpdateTodoDto} from './update-todo.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,28 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('/todos')
+  getAllTodos(): Todo[] {
+    return this.appService.getAllTodos()
+  }
+
+  @Post('/todos')
+  createTodo(@Body() createTodoDto: CreateTodoDto):Todo {
+    return this.appService.createTodo(createTodoDto);
+  }
+
+  @Delete('/todos/:id')
+  deleteTodoById(@Param('id') id: string) {
+    return this.appService.deleteTodoById(Number(id));
+  }
+
+  @Put('todos/:id')
+  updateTodo(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) :Todo | null {
+    return this.appService.updateTodo(Number(id), updateTodoDto);
+  }
+  @Patch('todos/:id')
+  updatePatchTodo(@Param('id') id: string, @Body()updateTodoDto: UpdateTodoDto ) :Todo | null {
+    return this.appService.updatePatchTodo(Number(id), updateTodoDto);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {CreateTodoDto} from './create-todo.dto';
+import {UpdateTodoDto} from './update-todo.dto';
+
+export interface Todo {
+    id: number;
+    title: string;
+    description: string;
+    date: Date;
+    completed: boolean;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    private todos: Todo[] = [];
+    private nextId: number = 1;
+
+    getHello(): string {
+        return 'Hello World!';
+    }
+
+    createTodo(createTodoDto: CreateTodoDto): Todo {
+        const newTodo: Todo = {
+            id: this.nextId++,
+            title: createTodoDto.title,
+            description: createTodoDto.description,
+            date: createTodoDto.date,
+            completed: false
+        };
+        this.todos.push(newTodo);
+        return newTodo;
+    }
+
+    getAllTodos(): Todo[] {
+        return this.todos;
+    }
+
+    deleteTodoById(id: number): Todo | null {
+        const index = this.todos.findIndex(todo => todo.id === id);
+        const victim = this.todos.at(index) as Todo;
+        if (index === -1) {
+            return null;
+        }
+        this.todos.splice(index, 1);
+        return victim;
+    }
+
+    updateTodo(id: number, updateTodoDto: UpdateTodoDto): Todo | null {
+        const todo = this.todos.find(todo => todo.id === id);
+        if (!todo) {
+            return null;
+        }
+        todo.title = updateTodoDto.title;
+        todo.description = updateTodoDto.description;
+        todo.date = updateTodoDto.date;
+        todo.completed = updateTodoDto.completed;
+        return todo;
+
+    }
+    updatePatchTodo(id: number, updateTodoDto: UpdateTodoDto): Todo | null {
+        const todo = this.todos.find(todo => todo.id === id);
+        if (!todo) {
+            return null;
+        }
+        // if (updateTodoDto.completed !== undefined)
+        todo.completed = updateTodoDto.completed;
+        return todo;
+    }
 }
